401785cb22643d9faf3ebe8e523c3f5514f420d5
Milotskiy Alexey <spalaxinco@gmail.com>
2013-07-01 23:13:27 +0300

    Removed useless files.

--- config/module/service_manager.config.php
+++
@@ -1,9 +0,0 @@
-<?php
-use Zend\ServiceManager\ServiceManager;
-
-return array(
-        'invokables' => array(),
-        'factories' => array(
-            'translator' => 'Zend\I18n\Translator\TranslatorServiceFactory'
-        )
-);
